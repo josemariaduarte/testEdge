228b1333f2177446fefb5e8575e9cf1b45d73b41
Jose Duarte <josemduarte91@gmail.com>
2020-06-12 11:13:59 -0400

    add index.html

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import path from 'path';
 
 const app=express();
 // usamos para desarrollo
@@ -9,9 +10,10 @@ app.use(cors('dev'));
 // 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // el puerto para correr
-app.set('port', process.env.PORT || 3001)
+app.set('port', process.env.PORT || 3002)
 
 app.listen(app.get('port'), ()=>{
   console.log('Corriendo en el puerto' + app.get('port'))
