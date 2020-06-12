8a1e77d6507ba3d616d605eb7883364a167072dd
Jose Duarte <josemduarte91@gmail.com>
2020-06-12 10:19:47 -0400

    Add dependencies morgan and cors

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+
 const express=require('express');
+const morgan=require('morgan');
+const cors=require('cors');
+
 const app=express();
+// usamos para desarrollo
+app.use(morgan('dev'));
+app.use(cors('dev'));
+// 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 // el puerto para correr
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3001)
 
 app.listen(app.get('port'), ()=>{
   console.log('Corriendo en el puerto' + app.get('port'))
